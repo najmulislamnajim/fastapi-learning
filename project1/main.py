1bb99dfb5f2508648cc4870d3822ea71066f51b3
najmulislamnajim <ninbd17@gmail.com>
2025-05-01 23:59:59 +0600

    add delete method

--- project1/main.py
+++ project1/main.py
@@ -30,3 +30,9 @@ async def update_book(new_book=Body()):
             books[i]=new_book
             break
         
+@app.delete('/delete_book/{book_title}')
+async def delete_book(book_title:str):
+    for i in range(len(books)):
+        if books[i].get('title').casefold() == book_title.casefold():
+            books.pop(i)
+            break
